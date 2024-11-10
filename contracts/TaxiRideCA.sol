a2b721eb05d4add533407754b8a2d1ebf8641ea9
2022429 <2022429@student.cct.ie>
2024-11-10 16:34:10 +0000

    Commting Taxi Ride

--- contracts/TaxiRideCA.sol
+++ contracts/TaxiRideCA.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.7;
 
 contract TaxiRideCA {
 
-    address public passenger;
-    address payable public driver;
-    uint public rideCost;
-    bool public rideCompleted;
+//Defining the roles 
+    address public passenger;    //Passanger
+    address payable public driver;    //Driver
+    uint public rideCost;   //cost of the ride
+    bool public rideCompleted;   
 }
